f59686a09a2f43f8a28ca241f8e61285dc78ca67
John Wells <john@daffinity.com>
2018-05-31 17:33:10 -0600

    Make source modules use HTTPS instead of SSH for better compatibility. Not all environments have SSH keys and therfore can't auth with GitHub.

--- examples/vault-cluster-public/main.tf
+++ examples/vault-cluster-public/main.tf
@@ -22,7 +22,7 @@ terraform {
 module "vault_cluster" {
   # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
   # to a specific version of the modules, such as the following example:
-  # source = "git::git@github.com:hashicorp/terraform-google-vault.git//modules/vault-cluster?ref=v0.0.1"
+  # source = "git::https://github.com/hashicorp/terraform-google-vault.git//modules/vault-cluster?ref=v0.0.4"
   source = "../../modules/vault-cluster"
 
   gcp_zone = "${var.gcp_zone}"
@@ -74,7 +74,7 @@ data "template_file" "startup_script_vault" {
 module "vault_load_balancer" {
   # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
   # to a specific version of the modules, such as the following example:
-  # source = "git::git@github.com:hashicorp/terraform-google-vault.git//modules/vault-lb-regional-ext?ref=v0.0.1"
+  # source = "git::https://github.com/hashicorp/terraform-google-vault.git//modules/vault-lb-fr?ref=v0.0.4"
   source = "../../modules/vault-lb-fr"
 
   cluster_name = "${var.vault_cluster_name}"
@@ -86,7 +86,7 @@ module "vault_load_balancer" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 module "consul_cluster" {
-  source = "git::git@github.com:hashicorp/terraform-google-consul.git//modules/consul-cluster?ref=v0.0.3"
+  source = "git::https://github.com/hashicorp/terraform-google-consul.git//modules/consul-cluster?ref=v0.0.3"
 
   gcp_zone = "${var.gcp_zone}"
   cluster_name = "${var.consul_server_cluster_name}"
