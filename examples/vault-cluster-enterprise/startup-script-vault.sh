c3cc8fc41a4c47d88073834df23560faee3189e5
Rob Morgan <robbym@gmail.com>
2018-10-02 19:12:53 +0200

    register vault enterprise and init correctly

--- examples/vault-cluster-enterprise/startup-script-vault.sh
+++ examples/vault-cluster-enterprise/startup-script-vault.sh
@@ -32,16 +32,17 @@ readonly VAULT_TLS_KEY_FILE="/opt/vault/tls/vault.key.pem"
 # Initializes a vault server
 # run-vault is running on the background and we have to wait for it to be done,
 # so in case this fails we retry.
+# Note: This script will run on all nodes so one node will initialize Vault and become the leader and the rest will fail.
 function retry_init {
   for i in $(seq 1 20); do
-    echo "Initializing Vault agent..."
+    echo "Initializing Vault..."
     # The boolean operations with the exit status are there to temporarily circumvent the "set -e" at the
     # beginning of this script which exits the script immediatelly for error status while not losing the exit status code
     server_output=$(/opt/vault/bin/vault operator init) && exit_status=0 || exit_status=$?
     if [[ $exit_status -eq 0 ]]; then
       return
     fi
-    echo "Failed to auth initialize Vault. Will sleep for 5 seconds and try again."
+    echo "Failed to initialize Vault. Will sleep for 5 seconds and try again."
     sleep 5
   done
 
@@ -51,13 +52,40 @@ function retry_init {
 
 retry_init
 
-# TODO - add license key to vault
+# The expected output should be similar to this:
+# ==========================================================================
+# Unseal Key 1: ddPRelXzh9BdgqIDqQO9K0ldtHIBmY9AqsTohM6zCRl7
+# Unseal Key 2: liSgypzdVrAxz73KbKyCMjVeSnRMuxCZMk1PWIZdjENS
+# Unseal Key 3: pmgeVu/fs8+jl8bOzf3Cq56BFufm4o7Sxt2oaUcvt6Dp
+# Unseal Key 4: i3W2xJEyUqUqcO1QSjTA+Ua0RUPxnNWM27AqaC8wW7Zh
+# Unseal Key 5: vHsQtCRgfblPeFYw1hhCVbji0MoNUP8zyIWhLWs3PebS
+#
+# Initial Root Token: cb076fc1-cc1f-6766-795f-b3822ba1ac57
+#
+# Vault initialized with 5 key shares and a key threshold of 3. Please securely
+# distribute the key shares printed above. When the Vault is re-sealed,
+# restarted, or stopped, you must supply at least 3 of these keys to unseal it
+# before it can start servicing requests.
+#
+# Vault does not store the generated master key. Without at least 3 key to
+# reconstruct the master key, Vault will remain permanently sealed!
+#
+# It is possible to generate new unseal keys, provided you have a quorum of
+# existing unseal keys shares. See "vault operator rekey" for more information.
+# ==========================================================================
 
-# Normally we would unseal Vault here with the generated keys, however we rely on the Auto Unseal
-# feature (https://www.vaultproject.io/docs/enterprise/auto-unseal/index.html) to do that for us automatically.
-/usr/bin/supervisorctl restart vault
+# Unseals the server with 3 keys from this output
+# Please note that this is not how it should be done in production as it is not secure and and we are
+# not storing any of the tokens.
+echo "$server_output" | head -n 3 | awk '{ print $4; }' | xargs -l /opt/vault/bin/vault operator unseal
 
-# Writes some secret, this secret is being written by terraform for test purposes
+# Exports the client token environment variable necessary for running the following vault commands
+export VAULT_TOKEN=$(echo "$server_output" | head -n 7 | tail -n 1 | awk '{ print $4; }')
+
+# Add the Enterprise license key to vault
+/opt/vault/bin/vault write /sys/license "text=$(cat /opt/vault/vault-license.hclic)"
+
+# Writes some secret, this secret is being written by Terraform for test purposes
 # Please note that normally we would never pass a secret this way
 # This is just so we can verify that our example instance is authenticating correctly
 /opt/vault/bin/vault write secret/example_gruntwork the_answer=${example_secret}
