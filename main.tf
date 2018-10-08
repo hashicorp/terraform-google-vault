7200b464a2c04b90c52010792b8945c48f908f01
Guy Newbury <guy.newbury@cloudbuy.com>
2018-10-08 12:40:16 +0100

    gcp_zone -> gcp_region for consul

--- main.tf
+++ main.tf
@@ -76,7 +76,7 @@ module "consul_cluster" {
 
   gcp_project_id     = "${var.gcp_project_id}"
   network_project_id = "${var.network_project_id}"
-  gcp_zone           = "${var.gcp_zone}"
+  gcp_region         = "${var.gcp_region}"
   cluster_name       = "${var.consul_server_cluster_name}"
   cluster_tag_name   = "${var.consul_server_cluster_name}"
   cluster_size       = "${var.consul_server_cluster_size}"
