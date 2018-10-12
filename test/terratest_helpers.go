62e4dfe82a9e0073e9931934d213976a766c2be6
Josh Padnick <josh.padnick@gmail.com>
2018-10-11 21:05:21 -0700

    Initial Vault public cluster tests [wip]

---
+++ test/terratest_helpers.go
@@ -0,0 +1,49 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/packer"
+	"github.com/gruntwork-io/terratest/modules/test-structure"
+)
+
+// PACKER_VAR_GCP_PROJECT_ID represents the Project ID variable in the Packer template
+const PACKER_VAR_GCP_PROJECT_ID = "project_id"
+
+// PACKER_VAR_GCP_ZONE represents the Zone variable in the Packer template
+const PACKER_VAR_GCP_ZONE = "zone"
+
+const PACKER_VAR_CA_PUBLIC_KEY       = "ca_public_key_path"
+const PACKER_VAR_TLS_PUBLIC_KEY      = "tls_public_key_path"
+const PAKCER_VAR_TLS_PRIVATE_KEY     = "tls_private_key_path"
+const PACKER_VAR_CONSUL_DOWNLOAD_URL = "CONSUL_DOWNLOAD_URL"
+const PACKER_VAR_VAULT_DOWNLOAD_URL  = "VAULT_DOWNLOAD_URL"
+
+const SAVED_TLS_CERT = "TlsCert"
+
+// Use Packer to build the Image in the given Packer template, with the given build name and return the Image ID.
+func buildVaultImage(t *testing.T, packerTemplatePath string, packerBuildName string, gcpProjectID string, gcpZone string, tlsCert TlsCert) string {
+	options := &packer.Options{
+		Template: packerTemplatePath,
+		Only:     packerBuildName,
+		Vars: map[string]string{
+			PACKER_VAR_GCP_PROJECT_ID:  gcpProjectID,
+			PACKER_VAR_GCP_ZONE:        gcpZone,
+			PACKER_VAR_CA_PUBLIC_KEY :  tlsCert.CAPublicKeyPath,
+			PACKER_VAR_TLS_PUBLIC_KEY:  tlsCert.PublicKeyPath,
+			PAKCER_VAR_TLS_PRIVATE_KEY: tlsCert.PrivateKeyPath,
+		},
+	}
+
+	return packer.BuildArtifact(t, options)
+}
+
+func saveTLSCert(t *testing.T, testFolder string, tlsCert TlsCert) {
+	test_structure.SaveTestData(t, test_structure.FormatTestDataPath(testFolder, SAVED_TLS_CERT), tlsCert)
+}
+
+func loadTLSCert(t *testing.T, testFolder string) TlsCert {
+	var tlsCert TlsCert
+	test_structure.LoadTestData(t, test_structure.FormatTestDataPath(testFolder, SAVED_TLS_CERT), &tlsCert)
+	return tlsCert
+}
---
+++ test/vault_cluster_public_test.go
@@ -0,0 +1,95 @@
+package test
+
+import (
+	"fmt"
+	"github.com/gruntwork-io/terratest/modules/gcp"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/gruntwork-io/terratest/modules/test-structure"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+// Terratest saved value names
+const SAVED_GCP_PROJECT_ID = "GcpProjectId"
+const SAVED_GCP_REGION_NAME = "GcpRegionName"
+const SAVED_GCP_ZONE_NAME = "GcpZoneName"
+
+// Terraform module vars
+const TFVAR_NAME_GCP_PROJECT_ID = "gcp_project_id"
+const TFVAR_NAME_GCP_REGION = "gcp_region"
+
+const TFVAR_NAME_VAULT_CLUSTER_NAME = "vault_cluster_name"
+const TFVAR_NAME_VAULT_SOURCE_IMAGE = "vault_source_image"
+const TFVAR_NAME_VAULT_CLUSTER_MACHINE_TYPE = "vault_cluster_machine_type"
+
+const TFVAR_NAME_CONSUL_SOURCE_IMAGE = "consul_server_source_image"
+const TFVAR_NAME_CONSUL_SERVER_CLUSTER_NAME = "consul_server_cluster_name"
+const TFVAR_NAME_CONSUL_SERVER_CLUSTER_MACHINE_TYPE = "consul_server_machine_type"
+
+func TestIntegrationVaultOpenSourcePublicClusterUbuntu(t *testing.T) {
+	t.Parallel()
+}
+
+func testVaultPublicCluster(t *testing.T, osName string, edition string) {
+	exampleDir := test_structure.CopyTerraformFolderToTemp(t, "../", ".")
+	vaultImageDir := filepath.Join(exampleDir, "examples", "vault-consul-image")
+
+	test_structure.RunTestStage(t, "build_image", func() {
+		gcpProjectId := gcp.GetGoogleProjectIDFromEnvVar(t)
+		gcpRegion := gcp.GetRandomRegion(t, gcpProjectId, nil, nil)
+		gcpZone := gcp.GetRandomZoneForRegion(t, gcpProjectId, gcpRegion)
+
+		test_structure.SaveString(t, exampleDir, SAVED_GCP_PROJECT_ID, gcpProjectId)
+		test_structure.SaveString(t, exampleDir, SAVED_GCP_REGION_NAME, gcpRegion)
+		test_structure.SaveString(t, exampleDir, SAVED_GCP_ZONE_NAME, gcpZone)
+
+		tlsCert := generateSelfSignedTlsCert(t)
+		saveTLSCert(t, vaultImageDir, tlsCert)
+
+		imageID := buildVaultImage(t, vaultImageDir, osName, gcpProjectId, gcpZone, tlsCert)
+		test_structure.SaveArtifactID(t, exampleDir, imageID)
+	})
+
+	defer test_structure.RunTestStage(t, "teardown", func() {
+		projectID := test_structure.LoadString(t, exampleDir, SAVED_GCP_PROJECT_ID)
+		imageName := test_structure.LoadArtifactID(t, exampleDir)
+
+		image := gcp.FetchImage(t, projectID, imageName)
+		image.DeleteImage(t)
+
+		tlsCert := loadTLSCert(t, exampleDir)
+		cleanupTLSCertFiles(tlsCert)
+	})
+
+	test_structure.RunTestStage(t, "deploy", func() {
+		gcpProjectId := test_structure.LoadString(t, exampleDir, SAVED_GCP_PROJECT_ID)
+		gcpRegion := test_structure.LoadString(t, exampleDir, SAVED_GCP_REGION_NAME)
+
+		// GCP only supports lowercase names for some resources
+		uniqueID := strings.ToLower(random.UniqueId())
+		imageID := test_structure.LoadArtifactID(t, exampleDir)
+
+		terraformOptions := &terraform.Options{
+			TerraformDir: exampleDir,
+			Vars: map[string]interface{}{
+				TFVAR_NAME_GCP_PROJECT_ID:                     gcpProjectId,
+				TFVAR_NAME_GCP_REGION:                         gcpRegion,
+				TFVAR_NAME_VAULT_CLUSTER_NAME:                 fmt.Sprintf("vault-test-%s", uniqueID),
+				TFVAR_NAME_VAULT_SOURCE_IMAGE:                 imageID,
+				TFVAR_NAME_VAULT_CLUSTER_MACHINE_TYPE:         "g1-small",
+				TFVAR_NAME_CONSUL_SERVER_CLUSTER_NAME:         fmt.Sprintf("consul-test-%s", uniqueID),
+				TFVAR_NAME_CONSUL_SOURCE_IMAGE:                imageID,
+				TFVAR_NAME_CONSUL_SERVER_CLUSTER_MACHINE_TYPE: "g1-small",
+			},
+		}
+		test_structure.SaveTerraformOptions(t, exampleDir, terraformOptions)
+
+		terraform.InitAndApply(t, terraformOptions)
+	})
+
+	test_structure.RunTestStage(t, "validate", func() {
+		// TODO: Fill this in.
+	})
+}
