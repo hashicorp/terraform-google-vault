138c89b03b5fa35fa0fef1c22deb9c2c5708f1e2
Rob Morgan <robbym@gmail.com>
2019-02-15 15:45:14 +0100

    fallback to cluster name if gcs_bucket_name is not specified

--- modules/vault-cluster/main.tf
+++ modules/vault-cluster/main.tf
@@ -275,7 +275,7 @@ resource "google_compute_firewall" "allow_inbound_health_check" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "google_storage_bucket" "vault_storage_backend" {
-  name          = "${var.gcs_bucket_name}"
+  name          = "${var.gcs_bucket_name != "" ? var.gcs_bucket_name : var.cluster_name}"
   location      = "${var.gcs_bucket_location}"
   storage_class = "${var.gcs_bucket_storage_class}"
   project       = "${var.gcp_project_id}"
