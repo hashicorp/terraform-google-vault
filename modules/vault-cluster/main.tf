80f27b3dec389297ceef25cf1915cf2f994acfaf
Dingan(Will) Wang <wwang@teradici.com>
2021-01-28 17:01:55 -0800

    Update the script for google provider 2.0.0
    update_strategy was obsolete.
    instance_template argument was updated.

--- modules/vault-cluster/main.tf
+++ modules/vault-cluster/main.tf
@@ -57,13 +57,20 @@ resource "google_compute_region_instance_group_manager" "vault" {
   project = var.gcp_project_id
 
   base_instance_name = var.cluster_name
-  instance_template  = data.template_file.compute_instance_template_self_link.rendered
+
+  version {
+    instance_template  = data.template_file.compute_instance_template_self_link.rendered
+  }
+
   region             = var.gcp_region
 
   # Restarting a Vault server has an important consequence: The Vault server has to be manually unsealed again. Therefore,
   # the update strategy used to roll out a new GCE Instance Template must be a rolling update. But since Terraform does
   # not yet support ROLLING_UPDATE, such updates must be manually rolled out for now.
-  update_strategy = var.instance_group_update_strategy
+  # Invalid after google provider 2.0.0
+  # may consider to use update_policy if needed.
+  # https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/version_2_upgrade
+  # update_strategy = var.instance_group_update_strategy
 
   target_pools = var.instance_group_target_pools
   target_size  = var.cluster_size
